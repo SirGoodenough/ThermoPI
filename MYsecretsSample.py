b48fa0608fce82558e57942f8600c3a5effcd490
Sir GoodEnough <james.chaloupka.1@gmail.com>
2021-11-03 13:45:34 -0500

    add PIN to secrets

--- MYsecretsSample.py
+++ MYsecretsSample.py
@@ -6,6 +6,6 @@ MQTT = {
     'TEMP' : 'MQTT temperature topic here, usually node/temperature where node is this device name',
     'HUMI' : 'MQTT humidity topic here, usually node/humidity where node is this device name',
     'LWT' : 'MQTT last will topic here, usually node/lwt where node is this device name',
-    'PIN' : 'The GPIO pin used to connect the device',
+    'PIN' : 4, # The GPIO pin used to connect the device
     'LOOP' : 300 # This needs to be a number.  Time in seconds for each loop.  Too fast breaks things so I suggest ~ 300 seconds IE 5 minutes.
     }

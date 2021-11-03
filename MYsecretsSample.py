ce468a61beb6a96198fa498ba96a1d9197c1f482
Sir GoodEnough <james.chaloupka.1@gmail.com>
2021-11-03 13:22:11 -0500

    Better explanation of variables

--- MYsecretsSample.py
+++ MYsecretsSample.py
@@ -1,10 +1,10 @@
 MQTT = {
     'HOST' : 'Broker IP Address Here',
-    'PORT' : 1883,
+    'PORT' : 'This needs to be a number, usually 1883 as the MQTT port for tcp access'
     'USER' : 'MQTT Username Here',
     'PWD' : 'MQTT User Password Here',
     'TEMP' : 'MQTT temperature topic here, usually node/temperature where node is this device name',
     'HUMI' : 'MQTT humidity topic here, usually node/humidity where node is this device name',
     'LWT' : 'MQTT last will topic here, usually node/lwt where node is this device name',
-    'LOOP' : 'Time in seconds for each loop.  Too fast breaks things so I suggest ~ 300 seconds IE 5 minutes.'
+    'LOOP' : 'This needs to be a number.  Time in seconds for each loop.  Too fast breaks things so I suggest ~ 300 seconds IE 5 minutes.'
     }

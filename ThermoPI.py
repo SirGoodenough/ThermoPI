f5936000cf53ebc58a596630b42cde816469fce3
Sir GoodEnough <james.chaloupka.1@gmail.com>
2023-05-20 18:02:23 -0500

    iterate

--- ThermoPI.py
+++ ThermoPI.py
@@ -189,7 +189,7 @@ def on2message(mqttc, userdata, msg):
         Angle = whSet
         print (f"Setting Motor to Angle: {Angle}")
         Duty = (Angle / 180) * PWC + PWM0
-        srvo.output(SERVOPIN, True)
+        GPIO.output(SERVOPIN, True)
         srvo.ChangeDutyCycle(Duty)
         time.sleep(1)
         srvo.stop()

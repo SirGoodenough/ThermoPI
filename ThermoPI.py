b1c07c9132f142f5dc659f69b6626d78375029f5
Sir GoodEnough <james.chaloupka.1@gmail.com>
2023-05-22 19:51:20 -0500

    payload fix

--- ThermoPI.py
+++ ThermoPI.py
@@ -157,7 +157,7 @@ def on2message(mqttc, userdata, msg):
     # The callback for when a PUBLISH message is received from the server.
 
     Topic = msg.topic
-    whSet = unpack('>f',msg.payload)
+    whSet = unpack('>l',msg.payload)
 
     print (f"Message: {str(whSet)} from Topic: {Topic}")
 
